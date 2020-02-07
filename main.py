c354de06167541622e07c331ad364f1415174a65
KlavsVoronovs <klavsvoronovs2@gmail.com>
2020-02-07 10:52:57 +0000

    ...

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from failproc import read_file
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "<a href='/home'>Hi!</a>"
+
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
 
 @app.route('/home')
 def home():
@@ -16,7 +21,15 @@ def about():
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html', phone = 123456789)
+  return render_template('contact.html', phone = 7442457)
+
+@app.route('/params')
+def params():
+  return request.args
+
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port = 5222, threaded = True, debug = True)
